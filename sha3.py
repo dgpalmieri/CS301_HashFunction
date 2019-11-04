06084f22fdcd16a258400a7a1059c4ebbd18f481
Dylan Palmieri <dgpalmieri@gmail.com>
2019-11-04 14:14:14 -0900

    Continue outline

--- sha3.py
+++ sha3.py
@@ -3,10 +3,19 @@
 # SHA-3 implementation
 # CS301 Fall 2019
 
-# 3 Functions
-#   Main SHA-3 compute
-#   Padding function
-#   Block permutation
+"""
+Functions
+    Main SHA-3 compute
+    Padding function
+    Block permutation
+
+Vars
+    Input bit string N
+    permutation block width b
+    rate r - size of read and write chunks (?)
+    output length d
+    array State = 5x5 array of shorts (64 bits)
+"""
 
 
 def compute_sha3():
