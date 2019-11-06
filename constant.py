592a5255564bd7e88be4aeb11a37a65efc83e8a6
Dylan Palmieri <dgpalmieri@gmail.com>
2019-11-06 12:40:42 -0900

    Add constants file with appropriate constants for SHA3-256

---
+++ constant.py
@@ -0,0 +1,9 @@
+# constant.py
+# Dylan and Keegan Palmieri
+# 11/6/19
+# Constants for SHA3-256
+
+BLOCK_WIDTH = 1600      # permutation block width b
+BIT_RATE = 1088         # rate r
+OUTPUT_LENGTH = 256     # output length d
+CAPACITY = 512          # capacity c
