02e9502cd51000afd756cfc7d8aa3e2845fa1b87
Dylan Palmieri <dgpalmieri@gmail.com>
2019-11-22 12:05:28 -0900

    Add word size

--- constant.py
+++ constant.py
@@ -3,7 +3,8 @@
 # 11/6/19
 # Constants for SHA3-256
 
-BLOCK_WIDTH = 1600      # permutation block width b
-BIT_RATE = 1088         # rate r
-OUTPUT_LENGTH = 256     # output length d
-CAPACITY = 512          # capacity c
+BLOCK_WIDTH = 1600               # permutation block width b
+BIT_RATE = 1088                  # rate r
+OUTPUT_LENGTH = 256              # output length d
+CAPACITY = 512                   # capacity c
+WORD_SIZE = int(BLOCK_WIDTH/25)  # word size l
