f5e2136ea16e74cf6fa4f12f1c452c4ffa9e8987
Dylan Palmieri <dgpalmieri@gmail.com>
2019-12-02 15:54:23 -0900

    Add l

--- constant.py
+++ constant.py
@@ -3,9 +3,11 @@
 # 11/6/19
 # Constants for SHA3-256
 
+from math import log2
+
 BLOCK_WIDTH = 1600               # permutation block width b
 BIT_RATE = 1088                  # rate r
 OUTPUT_LENGTH = 256              # output length d
 CAPACITY = 512                   # capacity c
 WORD_SIZE = int(BLOCK_WIDTH/25)  # word size w
-
+WORD_POWER = log2(WORD_SIZE)     # power of two l
