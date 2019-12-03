26ee66e893e268b442d5cde41fadea5aebc79b06
Dylan Palmieri <dgpalmieri@gmail.com>
2019-12-02 16:10:09 -0900

    Cast l to int

--- constant.py
+++ constant.py
@@ -5,9 +5,9 @@
 
 from math import log2
 
-BLOCK_WIDTH = 1600               # permutation block width b
-BIT_RATE = 1088                  # rate r
-OUTPUT_LENGTH = 256              # output length d
-CAPACITY = 512                   # capacity c
-WORD_SIZE = int(BLOCK_WIDTH/25)  # word size w
-WORD_POWER = log2(WORD_SIZE)     # power of two l
+BLOCK_WIDTH = 1600                  # permutation block width b
+BIT_RATE = 1088                     # rate r
+OUTPUT_LENGTH = 256                 # output length d
+CAPACITY = 512                      # capacity c
+WORD_SIZE = int(BLOCK_WIDTH/25)     # word size w
+WORD_POWER = int(log2(WORD_SIZE))   # power of two l
